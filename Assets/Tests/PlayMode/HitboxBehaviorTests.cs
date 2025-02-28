c0c1bb852627d2f17b943d8e2187fac42868c6a1
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-02-28 20:19:17 +0700

    update to TestPlayerCollisionFinishPoint

--- Assets/Tests/PlayMode/HitboxBehaviorTests.cs
+++ Assets/Tests/PlayMode/HitboxBehaviorTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.UIElements;
 using static HitboxConfigTests;
 using static UnityEngine.UI.Dropdown;
 
@@ -526,6 +527,7 @@ public class HitboxBehaviorTests
                     break;
                 }
                 duration += Time.deltaTime;
+                yield return null;
             }
             if (!isDeadAnimation)
             {
@@ -549,12 +551,202 @@ public class HitboxBehaviorTests
         yield return null;
     }
 
-    private void HideTraps()
+    [UnityTest]
+    public IEnumerator TestPlayerCollisionWithTrapUntilRunsOutOfLives()
     {
-        GameObject trapParent = GameObject.Find(TestHelper.TRAP_PARENT_NAME);
-        if (trapParent != null)
+        string logFileName = TestSettings.TestLogFileName;
+        string message = nameof(TestPlayerCollisionWithTrapUntilRunsOutOfLives);
+        TestLogger.Log(message, logFileName);
+        List<string> issues = new();
+
+        GameObject trapsParent = GameObject.Find(TestHelper.TRAP_PARENT_NAME);
+        Assert.IsNotNull(trapsParent, "Không tìm thấy trapsParent trong scene.");
+
+        GameObject selectedTrap = null;
+        foreach (Transform child in trapsParent.transform)
         {
-            trapParent.SetActive(false);
+            GameObject trap = child.gameObject;
+            TrapType trapType = TestHelper.GetTrapType(trap.name);
+            trap = trapType == TrapType.SpikedBall ? TestHelper.GetSpikedBallTrapObject(trap) : trap;
+            if (trapType == TrapType.NoDamage || trapType == TrapType.Unknown)
+                continue;
+            selectedTrap = trap;
+            break;
+        }
+
+        GameObject player = GameObject.Find(TestHelper.PLAYER_NAME);
+        GameObject cam = GameObject.FindObjectOfType<Camera>().gameObject;
+        Player playerScript = player.GetComponent<Player>();
+
+        while (playerScript.lives > 0)
+        {
+            int inititalLives = playerScript.lives;
+
+            player.transform.position = selectedTrap.transform.position;
+            cam.transform.position = player.transform.position + Vector3.back * 10;
+
+            yield return new WaitForFixedUpdate();
+
+            int livesAfterCollision = playerScript.lives;
+            int damage = inititalLives - livesAfterCollision;
+            int expectedDamage = 1;
+
+            if (livesAfterCollision == 0)
+            {
+                playerScript.StopAllCoroutines();
+            }
+
+            if (damage != expectedDamage)
+            {
+                string issue = $"Player va chạm với bẫy không nhận đúng thiệt hại khi còn {inititalLives} mạng. " +
+                    $"\nExpected: Damage = {expectedDamage}. " +
+                    $"\nActual: Damage = {damage}.";
+                issues.Add(issue);
+            }
+            if (playerScript.CanMove())
+            {
+                string issue = $"Player va chạm với bẫy không bị ngừng hoạt động khi còn {inititalLives} mạng. ";
+                issues.Add(issue);
+            }
+
+            float duration = 0f;
+            bool isDeadAnimation = false;
+            while (duration <= 1)
+            {
+                if (playerScript.playerSprite.color == playerScript.damagedColor)
+                {
+                    isDeadAnimation = true;
+                    break;
+                }
+                duration += Time.deltaTime;
+                yield return null;
+            }
+            if (!isDeadAnimation)
+            {
+                string issue = $"Player va chạm với bẫy không hiện dead animation khi còn {inititalLives} mạng. ";
+                issues.Add(issue);
+            }
+            yield return new WaitForSeconds(2);
+        }
+
+        if (issues.Count > 0)
+        {
+            string errorMessage = string.Join("\n", issues);
+            TestLogger.Log(errorMessage, logFileName);
+            Assert.Fail(errorMessage);
+        }
+        else
+        {
+            string successMessage = $"{nameof(TestPlayerCollisionWithTrapUntilRunsOutOfLives)} passed.";
+            TestLogger.Log(successMessage, logFileName);
+            Assert.Pass(successMessage);
+        }
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator TestPlayerCollisionWithNoDamegeTrap()
+    {
+        string logFileName = TestSettings.TestLogFileName;
+        string message = nameof(TestPlayerCollisionWithNoDamegeTrap);
+        TestLogger.Log(message, logFileName);
+
+        GameObject player = GameObject.Find(TestHelper.PLAYER_NAME);
+        dichuyen2 playerMovement = player.GetComponent<dichuyen2>();
+        string file = $"{TestSettings.SceneName}-{nameof(TestPlayerCollisionWithNoDamegeTrap)}-InputData";
+        LoadSimulatorInputData(file);
+
+        HideTraps(true);
+        HideMovingPlatforms();
+
+        GameObject cam = GameObject.FindObjectOfType<Camera>().gameObject;
+        List<string> issues = new();
+
+        foreach (var sequence in sequences)
+        {
+            player.transform.position = sequence.begin.position;
+            cam.transform.position = player.transform.position + Vector3.back * 10;
+            ProcessPlayerAction(playerMovement, sequence.begin);
+            yield return new WaitForSeconds(0.2f);
+            Vector3 prePos = player.transform.position;
+
+            yield return new WaitForSeconds(1.5f);
+
+            if (player.transform.position == prePos)
+            {
+                string issue = $"Player bị xuyên qua NoDamageTrap tại {sequence.begin.position} ";
+                issues.Add(issue);
+            }
+
+        }
+
+        if (issues.Count > 0)
+        {
+            string errorMessage = string.Join("\n", issues);
+            TestLogger.Log(errorMessage, logFileName);
+            Assert.Fail(errorMessage);
+        }
+        else
+        {
+            string successMessage = $"{nameof(TestPlayerCollisionWithNoDamegeTrap)} passed.";
+            TestLogger.Log(successMessage, logFileName);
+            Assert.Pass(successMessage);
+        }
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator TestPlayerCollisionFinishPoint()
+    {
+        string logFileName = TestSettings.TestLogFileName;
+        string message = nameof(TestPlayerCollisionFinishPoint);
+        TestLogger.Log(message, logFileName);
+
+        GameObject finishPoint = GameObject.Find(TestHelper.FINISH_POINT_NAME);
+        Assert.IsNotNull(finishPoint, "Không tìm thấy FinishPoint trong scene.");
+        Win win = finishPoint.GetComponent<Win>();
+
+        GameObject player = GameObject.Find(TestHelper.PLAYER_NAME);
+
+        player.transform.position = finishPoint.transform.position;
+        yield return null;
+        
+        if (!win.gameWinIU.activeInHierarchy)
+        {
+            string err = "Màn hình gameWin không hiển thị khi Player va chạm với FinishPoint.";
+            TestLogger.Log(err, logFileName);
+            Assert.Fail(err);
+        }
+
+        string successMessage = $"{nameof(TestPlayerCollisionFinishPoint)} passed.";
+        TestLogger.Log(successMessage, logFileName);
+        Assert.Pass(successMessage);
+
+        yield return null;
+    }
+
+    private void HideTraps(bool exceptNoDamage = false)
+    {
+        if (exceptNoDamage)
+        {
+            GameObject trapParent = GameObject.Find(TestHelper.TRAP_PARENT_NAME);
+            foreach (Transform child in trapParent.transform)
+            {
+                GameObject trap = child.gameObject;
+                TrapType trapType = TestHelper.GetTrapType(trap.name);
+                if (trapType != TrapType.NoDamage)
+                    trap.SetActive(false);
+            }
+        }
+        else
+        {
+            GameObject trapParent = GameObject.Find(TestHelper.TRAP_PARENT_NAME);
+            if (trapParent != null)
+            {
+                trapParent.SetActive(false);
+            }
         }
     }
 
